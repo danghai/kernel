10e702d03cec1994fa303d37c26d7bf9e8991bf1
hai <goldsea5191@gmail.com>
2017-09-08 13:40:12 -0700

    implement the open, read and write method

--- character_device_driver/README.md
+++ character_device_driver/README.md
@@ -31,25 +31,23 @@ The file_operations structure is defined in `linux/fs.h` and holds pointers to f
 
 ```c
 struct file_operations {
-       struct module *owner;
-       loff_t (*llseek) (struct file *, loff_t, int);
-       ssize_t (*read) (struct file *, char *, size_t, loff_t *);
-       ssize_t (*write) (struct file *, const char *, size_t, loff_t *);
-       int (*readdir) (struct file *, void *, filldir_t);
-       unsigned int (*poll) (struct file *, struct poll_table_struct *);
-       int (*ioctl) (struct inode *, struct file *, unsigned int, unsigned long);
-       int (*mmap) (struct file *, struct vm_area_struct *);
-       int (*open) (struct inode *, struct file *);
-       int (*flush) (struct file *);
-       int (*release) (struct inode *, struct file *);
-       int (*fsync) (struct file *, struct dentry *, int datasync);
-       int (*fasync) (int, struct file *, int);
-       int (*lock) (struct file *, int, struct file_lock *);
-    ssize_t (*readv) (struct file *, const struct iovec *, unsigned long,
-          loff_t *);
-    ssize_t (*writev) (struct file *, const struct iovec *, unsigned long,
-          loff_t *);
-    };
+   struct module *owner;
+   loff_t (*llseek) (struct file *, loff_t, int);
+   ssize_t (*read) (struct file *, char *, size_t, loff_t *);
+   ssize_t (*write) (struct file *, const char *, size_t, loff_t *);
+   int (*readdir) (struct file *, void *, filldir_t);
+   unsigned int (*poll) (struct file *, struct poll_table_struct *);
+   int (*ioctl) (struct inode *, struct file *, unsigned int, unsigned long);
+   int (*mmap) (struct file *, struct vm_area_struct *);
+   int (*open) (struct inode *, struct file *);
+   int (*flush) (struct file *);
+   int (*release) (struct inode *, struct file *);
+   int (*fsync) (struct file *, struct dentry *, int datasync);
+   int (*fasync) (int, struct file *, int);
+   int (*lock) (struct file *, int, struct file_lock *);
+   ssize_t (*readv) (struct file *, const struct iovec *, unsigned long,loff_t *);
+   ssize_t (*writev) (struct file *, const struct iovec *, unsigned long,loff_t *);
+};
 ```
 
 An instance of struct file_operations containing pointers to functions that are used to implement read, write, open, ... syscalls is commonly named `fops`
--- character_device_driver/char.c
+++ character_device_driver/char.c
@@ -8,6 +8,9 @@
 #include <linux/kernel.h>
 #include <linux/cdev.h>
 #include <linux/types.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
 #define DEVICE_CNT 7
 #define DEVICE_NAME "chardev"   /* Dev name as it appears in /proc/devices */
 
@@ -26,7 +29,7 @@ static ssize_t device_write (struct file *, const char __user *, size_t, loff_t 
 
 static dev_t dev; /* dev represents device numbers within the kernel*/
 static struct cdev my_cdev; /* Struct cdev represents char devices internally*/
-static int data   /* data pass as a parameter for syscal_val */
+static int data;   /* data pass as a parameter for syscal_val */
 module_param(data,int, S_IRUSR | S_IWUSR);
 
 
@@ -36,9 +39,87 @@ static struct file_operations fops = {
   .owner = THIS_MODULE,
   .read = device_read,
   .write = device_write,
-  .open = device_open,
-  .release = device_release
+  .open = device_open
 };
+
+/* Define the method open of fops*/
+static int device_open(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "Successfully opened! \n");
+    printk(KERN_INFO "Currently, the value of data: %d\n",data);
+    return 0;
+}
+
+/* Define the method read of fops
+  + filp : file pointer
+  + count: the size of the requested data transfer
+  + buff: points to the user buffer holding the data to be written or the empty buffer
+  where the newly read data should be placed
+  + offp: represent the current file position after successful completion of the system call*/
+static ssize_t device_read (struct file *filp, char __user *buff, size_t count, loff_t *offp)
+{
+    int ret;
+    if (*offp >= sizeof(int))
+      return 0;
+
+    if(!buff)                /* No buffer in user*/
+      return -EINVAL;
+
+   /* Transfer data from Kernel to user */
+    if (copy_to_user(buff,&data,sizeof(int)))
+      return -EFAULT;
+    ret = sizeof(int);
+    *offp += count;
+
+    /* Confirm that user got exact data */
+    printk (KERN_INFO "User got from us: %d \n",data);
+
+    return ret;
+}
+
+/* Define the method write of fops
+  + filp : file pointer
+  + count: the size of the requested data transfer
+  + buff: points to the user buffer holding the data to be written or the empty buffer
+  where the newly read data should be placed
+  + offp: represent the current file position after successful completion of the system call*/
+static ssize_t device_write(struct file *filp, const char __user *buff, size_t count, loff_t *offp)
+{
+    /* local kernel memory*/
+    char *kern_buf;
+    int ret;
+
+    if(!buff)                /* No buffer in user*/
+      return -EINVAL;
+
+    /* Allocate memory in kernel */
+    kern_buf = kmalloc (count, GFP_KERNEL);
+    if (!kern_buf)
+      return -ENOMEM;
+
+    /* Transfer data from user to kernel through kernel buffer*/
+    if(copy_from_user(kern_buf,buff,count))
+    {
+        kfree(kern_buf);
+        return -EFAULT;
+    }
+
+    /* Update global data in kernel */
+    if(kstrtoint(kern_buf,10,&data))
+    {
+        kfree(kern_buf);
+        return -ERANGE;
+    }
+    ret = count;
+    /* Print result what userspace gave back the new value by system call */
+    printk(KERN_INFO "Good! Userspace wrote back the new data: %d to kernel \n",data);
+
+    return ret;
+}
+
+
+
+
 
 static int __init char_dev_init (void)
 {
