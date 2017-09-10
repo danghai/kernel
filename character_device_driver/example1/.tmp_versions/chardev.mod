eaa1c6df5998319d61dfc5f1b149e54e30d7b265
hai <goldsea5191@gmail.com>
2017-09-10 16:08:28 -0700

    update example1

---
+++ character_device_driver/example1/.tmp_versions/chardev.mod
@@ -0,0 +1,2 @@
+/home/danghai/Kernel/character_device_driver/example1/chardev.ko
+/home/danghai/Kernel/character_device_driver/example1/chardev.o
--- character_device_driver/example1/chardev.c
+++ character_device_driver/example1/chardev.c
@@ -5,9 +5,14 @@
 *   Modify: Hai Dang Hoang (Email: goldsea5191@gmail.com)
 */
 
-#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/cdev.h>
+#include <linux/types.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
 #include <asm/uaccess.h>
 
 MODULE_AUTHOR ("Hai Dang Hoang");
@@ -50,25 +55,88 @@ static int device_open(struct inode *inode, struct file *file)
     static int counter = 0;
     if(Device_Open)
         return -EBUSY;
+
+    Device_Open++;
+    sprintf(msg, "I already told you %d times Hello world! \n",counter++);
+    msg_Ptr = msg;
+    try_module_get(THIS_MODULE);
+
+    return SUCCESS;
 }
 
+/*
+* Called when a process closes the device file
+*/
+
+static int device_release(struct inode *inode, struct file *file)
+{
+    Device_Open--;    /* We're now ready for our next caller*/
+    /* Decrement the usage count, or else once you opened the file, you'll
+     never get rid of the module. */
+    module_put(THIS_MODULE);
+
+    return SUCCESS;
+}
+
+/*
+* Called when a process, which already opened the dev file, attempts to read from it
+*/
+
+static ssize_t device_read (struct file *filp, char *buffer, size_t length, loff_t *offset)
+{
+
+    /* Number of bytes actually written to the buffer */
+    int bytes_read = 0;
+
+    /* If we're at the end of the message, return 0 signifying end of file */
+    if(*msg_Ptr == 0)
+      return 0;
+
+    /* Acually put the data into the buffer */
+    while (length && *msg_Ptr)
+    {
+      /*
+      *   The buffer is in the user data segment, not the kernel
+      *   segment so "*" assignment won't work. We have to use
+      *   put_user which copies data from the kernel data segment to
+      *   the user data segment.
+      */
+
+      put_user(*(msg_Ptr++), buffer++);
+
+      length--;
+      bytes_read++;
+    }
+
+    /* Read functions return the number of bytes put into the buffer */
+    return bytes_read; 
+}
+
+/*
+*   Called when a process writes to dev file: echo "hi" > /dev/chardev
+*/
+static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+{
+    printk(KERN_ALERT "Sorry, this operation is not supported .\n");
+    return -EINVAL;
+}
 /*
 *   This function is called when the module is loaded
 */
 static int __init char_dev_init(void)
 {
     printk(KERN_INFO "Module loading and initial setup character driver .... \n");
-    Major = register_chrdev(0, DEVICE_NAME, *fops);
+    Major = register_chrdev(0, DEVICE_NAME, &fops);
     if(Major < 0)
     {
-      prink (KERN_ALERT "Registering char device failed with %d \n", Major);
+      printk (KERN_ALERT "Registering char device failed with %d \n", Major);
       return Major;
     }
 
     printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
     printk(KERN_INFO "the driver, create a dev file with \n");
     printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n",DEVICE_NAME, Major);
-    printk(KERN_INFO "Try various minor numbers. Ty to cat and echo to \n");
+    printk(KERN_INFO "Try various minor numbers. Try to cat and echo to \n");
     printk(KERN_INFO "the device file.\n");
     printk(KERN_INFO "Remove the device file and module when done .\n");
 
@@ -81,9 +149,7 @@ static int __init char_dev_init(void)
 static void __exit char_dev_exit(void)
 {
     /* Unregister the device */
-    int ret = unregister_chrdev (Major, DEVICE_NAME);
-    if(ret < 0)
-        printk(KERN_ALERT "Error in unregister_chrdev: %d\n", ret);
+    unregister_chrdev(Major, DEVICE_NAME);
     printk(KERN_INFO "Module unloaded! ...\n");
 }
 
